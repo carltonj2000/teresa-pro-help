ae0b4770ac63ae5701dab2455ba4d7b40c1297f7
Carlton Joseph <carlton.joseph@gmail.com>
2025-02-28 10:18:06 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Teresa's Professional Website Help
+
+```bash
+npm create vite@latest
+cd teresa-pro-help
+npm i
+npm install tailwindcss @tailwindcss/vite
+```
---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+function App() {
+  return (
+    <div className="max-w-xl mx-auto">
+      <div className="max-w-xl mx-auto bg-gray-50 p-3 rounded-xl shadow-lg text-gray-800 mt-2">
+        <h1 className="text-xl text-center mt-3 mb-2 font-bold">PWA</h1>
+        <p className="text-justify">
+          Progressive Web Apps (PWAs) are websites that allow you to install
+          themselves locally on your device(s) so that they are accessible when
+          there is no connectivity or your device is in airplane mode.
+        </p>
+        <h1 className="text-xl text-center mt-3 mb-2">iOS PWA</h1>
+        <p>
+          If you open a website that is a PWA on the Safari web browser you can
+          install it by clicking on the share icon, scrolling down a bit and
+          selecting add to home screen.
+        </p>
+        <h1 className="text-xl text-center mt-3 mb-2">Android PWA</h1>
+        <p className="text-justify">
+          If you open a website that is a PWA on the chrome web browser you will
+          be prompted to install it on your desktop or you can install it by
+          clicking on the install icon in the address bar in the top right.
+        </p>
+        <h1 className="text-xl text-center mt-3 mb-2">Content</h1>
+        <p>
+          There is other content on this website but no direct links are
+          provided here.
+        </p>
+      </div>
+      <div className="max-w-xl mx-auto bg-gray-50 p-3 rounded-xl shadow-lg text-gray-800 mt-2">
+        <h1 className="text-xl text-center mt-3 mb-2 font-semibold">
+          iOS PWA Details
+        </h1>
+        <p className="text-justify">Coming Soon ...</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
